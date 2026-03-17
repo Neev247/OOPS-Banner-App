f6d8802af3b8528f0aae2f5382116d2cff8ddea5
Neev247 <neev0247@gmail.com>
2026-03-17 09:40:12 +0530

    Implemetn UC2: Display OOPS as 7-line ASCII banner

---
+++ UC2.java
@@ -0,0 +1,13 @@
+public class UC2 {
+
+    public static void main(String[] args) {
+        // Using string concatenation to join the letters on each of the 7 lines
+        System.out.println("   *** " + "   " + "   *** " + "   " + " ****** " + "   " + "  ***** ");
+        System.out.println("  * * " + "   " + "  * * " + "   " + " * * " + "   " + " * * ");
+        System.out.println(" * * " + "   " + " * * " + "   " + " * * " + "   " + " * ");
+        System.out.println(" * * " + "   " + " * * " + "   " + " ****** " + "   " + "  ***** ");
+        System.out.println(" * * " + "   " + " * * " + "   " + " * " + "   " + "       * ");
+        System.out.println("  * * " + "   " + "  * * " + "   " + " * " + "   " + " * * ");
+        System.out.println("   *** " + "   " + "   *** " + "   " + " * " + "   " + "  ***** ");
+    }
+}
