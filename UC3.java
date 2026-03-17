fe17808e8326960a1994f65fceac4f3ca8b15da1
Neev247 <neev0247@gmail.com>
2026-03-17 14:11:48 +0530

    Update UC3.java

--- UC3.java
+++ UC3.java
@@ -1,14 +1,25 @@
-public class UC3 {
-
+public class OOPSBannerAppUC3 {
     public static void main(String[] args) {
-        // Using String.join() with a 3-space delimiter ("   ") 
-        // to efficiently combine the characters for each line.
-        System.out.println(String.join("   ", "  *** ", "  *** ", " ***** ", "  **** "));
-        System.out.println(String.join("   ", " * * ", " * * ", " * *", " * "));
-        System.out.println(String.join("   ", " * * ", " * * ", " * *", " * "));
-        System.out.println(String.join("   ", " * * ", " * * ", " ***** ", "  *** "));
-        System.out.println(String.join("   ", " * * ", " * * ", " * ", "     * "));
-        System.out.println(String.join("   ", " * * ", " * * ", " * ", "     * "));
-        System.out.println(String.join("   ", "  *** ", "  *** ", " * ", " **** "));
+
+        System.out.println(String.join("",
+                "  *****  ", "  *****  ", " ******  ", "  *****  "));
+
+        System.out.println(String.join("",
+                " **   ** ", " **   ** ", " **   ** ", " **   ** "));
+
+        System.out.println(String.join("",
+                "**     **", "**     **", " **   ** ", " **      "));
+
+        System.out.println(String.join("",
+                "**     **", "**     **", " ******  ", "  *****  "));
+
+        System.out.println(String.join("",
+                "**     **", "**     **", " **      ", "      ** "));
+
+        System.out.println(String.join("",
+                " **   ** ", " **   ** ", " **      ", " **   ** "));
+
+        System.out.println(String.join("",
+                "  *****  ", "  *****  ", " **      ", "  *****  "));
     }
 }
