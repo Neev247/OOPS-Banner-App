08a2169db392ac9d01d6f71122a9aa8531e3d714
Neev247 <neev0247@gmail.com>
2026-03-17 14:19:07 +0530

    Update UC8.java

--- UC8.java
+++ UC8.java
@@ -1,60 +1,69 @@
 import java.util.HashMap;
-import java.util.Map;
+
 public class UC8 {
 
-    public static void main(String[] args) {
-        // Initialize the HashMap to store our character-to-pattern mappings
-        Map<Character, String[]> patternMap = new HashMap<>();
+    public static HashMap<Character, String[]> createCharacterMap() {
+        HashMap<Character, String[]> charMap = new HashMap<>();
 
-        // Define and store the pattern for 'O'
-        patternMap.put('O', new String[] {
-            "  *** ", " * * ", " * * ", " * * ", " * * ", " * * ", "  *** "
+        charMap.put('O', new String[] {
+            "  *****   ",
+            " *     *  ",
+            " *     *  ",
+            " *     *  ",
+            " *     *  ",
+            " *     *  ",
+            "  *****   "
         });
 
-        // Define and store the pattern for 'P'
-        patternMap.put('P', new String[] {
-            " ***** ", " * *", " * *", " ***** ", " * ", " * ", " * "
+        charMap.put('P', new String[] {
+            "  *****   ",
+            " *     *  ",
+            " *     *  ",
+            " *****    ",
+            " *        ",
+            " *        ",
+            " *        "
         });
 
-        // Define and store the pattern for 'S'
-        patternMap.put('S', new String[] {
-            "  **** ", " * ", " * ", "  *** ", "     * ", "     * ", " **** "
+        charMap.put('S', new String[] {
+            "  *****   ",
+            " *        ",
+            " *        ",
+            "  *****   ",
+            "       *  ",
+            " *     *  ",
+            "  *****   "
         });
 
-        // The target text to display
-        String targetWord = "OOPS";
-        String[] bannerLines = new String[7];
+        charMap.put(' ', new String[] {
+            "          ",
+            "          ",
+            "          ",
+            "          ",
+            "          ",
+            "          ",
+            "          "
+        });
 
-        // Construct the banner using StringBuilder and HashMap retrieval
-        for (int i = 0; i < 7; i++) {
-            StringBuilder rowBuilder = new StringBuilder();
-            
-            for (int j = 0; j < targetWord.length(); j++) {
-                char currentChar = targetWord.charAt(j);
-                
-                // Efficient lookup using the HashMap
-                String[] pattern = patternMap.get(currentChar);
-                
-                // If the character exists in our map, append its row
-                if (pattern != null) {
-                    rowBuilder.append(pattern[i]);
-                } else {
-                    // Fallback spacing if a character isn't in our map
-                    rowBuilder.append("       ");
-                }
-                
-                // Add the 3-space delimiter between letters, but not after the last letter
-                if (j < targetWord.length() - 1) {
-                    rowBuilder.append("   ");
-                }
+        return charMap;
+    }
+
+    public static void displayBanner(String message, HashMap<Character, String[]> charMap) {
+        int patternHeight = charMap.get('O').length;
+
+        for (int line = 0; line < patternHeight; line++) {
+            StringBuilder sb = new StringBuilder();
+            for (char ch : message.toCharArray()) {
+                String[] pattern = charMap.getOrDefault(ch, charMap.get(' '));
+                sb.append(pattern[line]).append(" ");
             }
-            // Store the fully constructed row
-            bannerLines[i] = rowBuilder.toString();
+            System.out.println(sb.toString());
         }
+    }
 
-        // Display the final banner
-        for (String line : bannerLines) {
-            System.out.println(line);
-        }
+    public static void main(String[] args) {
+        HashMap<Character, String[]> charMap = createCharacterMap();
+        String message = "OOPS";
+        displayBanner(message, charMap);
     }
 }
