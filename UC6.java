ecbf889f2e58422357fb9cbe2c76214e4847f13c
Neev247 <neev0247@gmail.com>
2026-03-17 14:17:16 +0530

    Update UC6.java

--- UC6.java
+++ UC6.java
@@ -1,61 +1,54 @@
-public class UC6 {
+class UC6 {
 
-    // Static helper method for the letter 'O'
-    public static String[] getO() {
-        return new String[] {
-            "  *** ",
-            " * * ",
-            " * * ",
-            " * * ",
-            " * * ",
-            " * * ",
-            "  *** "
+    public static String[] getOPattern() {
+        return new String[]{
+            " *** ",
+            "*   *",
+            "*   *",
+            "*   *",
+            "*   *",
+            "*   *",
+            " *** "
         };
     }
 
-    // Static helper method for the letter 'P'
-    public static String[] getP() {
-        return new String[] {
-            " ***** ",
-            " * *",
-            " * *",
-            " ***** ",
-            " * ",
-            " * ",
-            " * "
+    public static String[] getPPattern() {
+        return new String[]{
+            "**** ",
+            "*   *",
+            "*   *",
+            "**** ",
+            "*    ",
+            "*    ",
+            "*    "
         };
     }
 
-    // Static helper method for the letter 'S'
-    public static String[] getS() {
-        return new String[] {
-            "  **** ",
-            " * ",
-            " * ",
-            "  *** ",
-            "     * ",
-            "     * ",
-            " **** "
+    public static String[] getSPattern() {
+        return new String[]{
+            " ****",
+            "*    ",
+            "*    ",
+            " *** ",
+            "    *",
+            "    *",
+            "**** "
         };
     }
 
     public static void main(String[] args) {
-        // Fetch the character arrays
-        String[] o = getO();
-        String[] p = getP();
-        String[] s = getS();
 
-        // Initialize the final banner array
-        String[] bannerLines = new String[7];
+        String[] oPattern = getOPattern();
+        String[] pPattern = getPPattern();
+        String[] sPattern = getSPattern();
 
-        // Loop through the 7 lines, joining the specific pieces for O-O-P-S
-        for (int i = 0; i < 7; i++) {
-            bannerLines[i] = String.join("   ", o[i], o[i], p[i], s[i]);
-        }
-
-        // Output the final banner
-        for (String line : bannerLines) {
-            System.out.println(line);
+        for (int i = 0; i < oPattern.length; i++) {
+            System.out.println(
+                    oPattern[i] + "  " +
+                    oPattern[i] + "  " +
+                    pPattern[i] + "  " +
+                    sPattern[i]
+            );
         }
     }
 }
