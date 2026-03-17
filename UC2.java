408a67a8eabea778b68f274b5bb33aa525b7eff5
Neev247 <neev0247@gmail.com>
2026-03-17 14:09:00 +0530

    Update UC2.java

--- UC2.java
+++ UC2.java
@@ -1,13 +1,12 @@
 public class UC2 {
-
     public static void main(String[] args) {
-        // Using string concatenation to join the letters on each of the 7 lines
-        System.out.println("   *** " + "   " + "   *** " + "   " + " ****** " + "   " + "  ***** ");
-        System.out.println("  * * " + "   " + "  * * " + "   " + " * * " + "   " + " * * ");
-        System.out.println(" * * " + "   " + " * * " + "   " + " * * " + "   " + " * ");
-        System.out.println(" * * " + "   " + " * * " + "   " + " ****** " + "   " + "  ***** ");
-        System.out.println(" * * " + "   " + " * * " + "   " + " * " + "   " + "       * ");
-        System.out.println("  * * " + "   " + "  * * " + "   " + " * " + "   " + " * * ");
-        System.out.println("   *** " + "   " + "   *** " + "   " + " * " + "   " + "  ***** ");
+
+        System.out.println("  *****  " + "  *****  " + " ******  " + "  *****  ");
+        System.out.println(" **   ** " + " **   ** " + " **   ** " + " **   ** ");
+        System.out.println("**     **" + "**     **" + " **   ** " + " **      ");
+        System.out.println("**     **" + "**     **" + " ******  " + "  *****  ");
+        System.out.println("**     **" + "**     **" + " **      " + "      ** ");
+        System.out.println(" **   ** " + " **   ** " + " **      " + " **   ** ");
+        System.out.println("  *****  " + "  *****  " + " **      " + "  *****  ");
     }
 }
